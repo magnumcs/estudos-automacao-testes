4efea4588f9301d587a4d8e76324110a266478e4
Magnum Adelano <magnemcs@gmail.com>
2019-03-29 00:11:45 -0400

    Commit 03

---
+++ features/step_definitions/contexto.rb
@@ -0,0 +1,20 @@
+Dado("que eu tenho {int} laranjas na bolsa.") do |_laranjas|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Quando("eu coloco {int} laranjas na bolsa") do |int|
+pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Então("eu verifico se o total de laranjas na bolsa é {int}.") do |int|
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("eu tiro {int} laranjas da bolsa") do |int|
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("eu verifico com quantas laranjas eu fiquei na bolsa.") do
+pending # Write code here that turns the phrase above into concrete actions
+end
+  
