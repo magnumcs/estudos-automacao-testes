e9e54bc1fd6c2991a51b85ca7e572ebbc96a7cef
mfaria <mfaria@fazenda>
2019-04-02 16:45:16 -0400

    Commit rapido

---
+++ automacao-cucumber-capybara-rspec/features/step_definitions/acessando_site.rb
@@ -0,0 +1,9 @@
+Quando("acesso a url") do
+    visit('/treinamento/home')
+    sleep(5)
+end
+
+Então("eu verifico se estou na página correta.") do
+pending # Write code here that turns the phrase above into concrete actions
+end
+  
---
+++ automacao-cucumber-capybara-rspec/features/support/env.rb
@@ -0,0 +1,11 @@
+require 'capybara/cucumber'
+require 'selenium-webdriver'
+
+Capybara.configure do |config|
+    #selenium/selenium_chrome/selenium_chrome_healess
+    #selenium-chrome
+    config.default_driver = :selenium_chrome
+    config.app_host = 'http://automacaocombatista.herokuapp.com'
+    config.default_max_wait_time = 5
+
+end
